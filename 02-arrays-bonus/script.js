efb897ccac04f3cf976d1492b147f52f149850b3
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-07-19 09:31:46 +0200

    add ex01 bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -8,19 +8,62 @@ const teachers = [
   'Luca'
 ]; // NON MODIFICARE QUESTA VARIABILE
 
+
+
+//===========================================================================================================
+
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+
+const reversedTeachers = teachers.reverse();
+
+
+/* const reversedTeachers = []; // creiamo un nuovo array
+for (let i = teachers.length - 1; i >= 0; i--) {
+  const teacher = teachers[i]; // recuperiamo il teacher in posizione i
+  console.log(teacher);        // stampiamo a video
+  reversedTeachers.push(teacher); // lo aggiungiamo al nuovo array
+}
+console.log(reversedTeachers); // stampiamo l’array finale invertito */
+
+
+
+
+//===========================================================================================================
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
+
 const longNames = null;
+
+
+
+
+
+
+//===========================================================================================================
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
+
+
+
+
+
+//===========================================================================================================
+
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
+
 const isFabioPresent = null;
 
+
+
+
+
+
+//===========================================================================================================
+
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
+
 const teachersString = null;
